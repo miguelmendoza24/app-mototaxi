15e611048b3d557c6e79bd1ad28a3e6b4f037397
miguel <miguelevamendo@gmail.com>
2026-09-19 13:14:21 -0600

    feat: configure React Router and dashboard navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Dashboard from './pages/Dashboard'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 
 function App() {
   return (
-    <>
-    <Navbar/>
-<Dashboard/>
-        </>
-  )
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
